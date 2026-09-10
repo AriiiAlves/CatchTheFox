240d67c0d24cb82f7347926a8e997237f4a224d3
Ariel Alves <alvzariel@gmail.com>
2026-09-10 10:46:43 -0300

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,7 +23,7 @@ const config = {
   url: 'https://ariiialves.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/manual-pwning/',
+  baseUrl: '/CatchTheFox/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
